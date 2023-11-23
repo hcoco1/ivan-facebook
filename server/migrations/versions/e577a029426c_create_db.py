67287c08dedef66af651bb3c85aa7f32dfc54aa8
Ivan <arias.ivan@gmail.com>
2023-11-22 22:38:30 -0500

    user model created

---
+++ server/migrations/versions/e577a029426c_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: e577a029426c
+Revises: 
+Create Date: 2023-11-22 22:14:02.679386
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e577a029426c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- server/models.py
+++ server/models.py
@@ -1,6 +1,25 @@
 from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.ext.associationproxy import association_proxy
-
+from sqlalchemy.orm import validates
 from config import db
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey, Table, Float, DateTime, String, Integer, Column
+from datetime import datetime
 
-# Models go here!
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+  
+
+    def to_dict(self):
+        return {"id": self.id, "username": self.username, "email": self.email}
+
+    def __repr__(self):
+        return f"<User(id={self.id}, username={self.username}, email={self.email})>"
